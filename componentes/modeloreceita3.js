4193e763dbc49c293efbf7dc2c6db20cfd5ed1b4
leticia <lethy.ribi15@gmail.com>
2021-09-14 17:04:35 -0300

    rc3

--- componentes/modeloreceita3.js
+++ componentes/modeloreceita3.js
@@ -1,20 +1,40 @@
 import Menu from './menu'
 import Footer from './footer'
-
+import Image from 'next/image'
 
 
 function ModeloReceita3(){
     return (
         
         <>
+         <Image src="/foto3.png" width={400} height={280} />
         
+         <h1>Brigadeiro de Abacaxi</h1>
+
+        <h2>INGREDIENTES</h2>
 
 
-<p>fazer o texto da receita com a foto e modo de preparo Receita 3
+<p>
+<ul>
+    <li>2 xícaras de abacaxi picado</li>
+    <li>1 lata de leite condensado</li>
+    <li>1 xícara de açúcar</li>
+    <li>1 xícara de coco ralado</li>
+    <li>Açúcar cristal ou coco ralado</li >
     
+
+</ul>
+
+<h2>MODO  DE  PREPARO</h2>
+
+<ul>
+    <li>Em uma panela misture o abacaxi, o leite condensado, o açúcar e coco ralado.</li>
+    <li>Leve ao fogo e mexa sempre por aproximadamente 30 minutos, até desprender do fundo da panela. </li>
+    <li>Coloque em um recipiente e espere esfriar. </li>
+    <li>Faça bolinhas com a mão untada com água, passe no açúcar cristal ou coco ralado e coloque em forminhas de brigadeiro.</li>
+   
+</ul>
 </p>
-
-
 
         </>
     )
