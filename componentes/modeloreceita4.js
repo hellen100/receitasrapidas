677bab1fbe2bfe2093bb050eb0f681a3a0fd178e
leticia <lethy.ribi15@gmail.com>
2021-09-14 17:14:33 -0300

    rc4

--- componentes/modeloreceita4.js
+++ componentes/modeloreceita4.js
@@ -1,5 +1,6 @@
 import Menu from './menu'
 import Footer from './footer'
+import Image from 'next/image'
 
 
 
@@ -7,11 +8,37 @@ function ModeloReceita4(){
     return (
         
         <>
+         <Image src="/foto4.png" width={400} height={280} />
         
+        <h1>PIMENTÃO RECHEADO COM QUEIJO COTTAGE</h1>
+
+       <h2>INGREDIENTES</h2>
 
 
-<p>fazer o texto da receita com a foto e modo de preparo Receita 4
-    
+
+<p>
+<ul>
+    <li>1 pimentão grande</li>
+    <li>150 g de queijo cottage - cerca de uma xícara</li>
+    <li>2 colheres de sopa de molho de tomate</li>
+    <li>2 fatias de muçarela</li>
+    <li>Ervas à gosto</li >
+    <li>Sal</li>
+    <li>Pimenta do reino</li >
+    <li></li >
+
+</ul>
+<h2>MODO  DE  PREPARO</h2>
+
+<ul>
+    <li>Pré-aqueça o forno a 200ºC</li>
+    <li>Limpe o pimentão, corte o top, remova o interior e faça uma "base" de queijo muçarela</li>
+    <li>Em uma tigela, misture o queijo cottage, o molho de tomate, as ervas (eu usei manjericão e salsinha), sal e pimenta do reino à gosto.</li>
+    <li>Depois, coloque o recheio no pimentão e cubra com uma fatia de queijo muçarela. Leve ao forno por 20 minutos</li>
+    <li>Bom apetite!</li>
+
+
+</ul>
 </p>
 
 
