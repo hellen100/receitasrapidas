86407c958d2c0e62b9c0f29d5cfe6c5b587d1d31
leticia <lethy.ribi15@gmail.com>
2021-09-15 14:08:46 -0300

    rc5

--- componentes/modeloreceita5.js
+++ componentes/modeloreceita5.js
@@ -1,17 +1,55 @@
 import Menu from './menu'
 import Footer from './footer'
-
+import Image from 'next/image'
 
 
 function ModeloReceita5(){
     return (
         
         <>
+         <Image src="/torta.png" width={400} height={280} />
         
+        <h1>Torta de Morango</h1>
+
+       <h2>INGREDIENTES</h2>
 
 
-<p>fazer o texto da receita com a foto e modo de preparo Receita 5
-    
+
+<p>
+<ul>
+    <li>1/2 xícara de chá de margarina ou manteiga</li>
+    <li>1 xícara de chá de farinha de trigo</li>
+    <li>1/2 xícara de chá de Amido de Milho Maizenamaizena Logo</li>
+    <li>1 gema</li>
+    <li>1 colher de sopa de açúcar</li >
+    <li>2 colheres de chá de essência de baunilha</li>
+    <li>1/2 xícara de chá de Amido de Milho Maizenamaizena Logo</li >
+    <li>3 xícaras de chá de leite</li >
+    <li>1/2 xícara de chá de açúcar</li >
+    <li>1/2 tablete de chocolate branco picado Cobertura</li>
+    <li>1 caixa de morango picado</li >
+    <li>1/2 tablete de chocolate branco picado</li>
+
+</ul>
+<h2>MODO  DE  PREPARO</h2>
+
+<ul>
+    <li>Em uma tigela média, junte a margarina, a farinha de trigo e o amido de milho Maizena.Misture.</li>
+    <li>Acrescente a gema, o açúcar e a essência de baunilha.</li>
+    <li>Misture e amasse delicadamente com a ponta dos dedos até obter uma massa homogênea.</li>
+    <li>Molde no formato de uma bola, embrulhe em filme plástico e reserve na geladeira por 15 minutos.</li>
+    <li>Aqueça o forno em temperatura média (180º C).</li>
+    <li>Em uma superfície seca e enfarinhada, abra a massa na espessura de 0,5 cm.</li>
+    <li>Forre o fundo e a lateral de uma fôrma desmontável (24cm de diâmetro).</li>
+    <li>Leve ao forno médio por 20 minutos ou até dourar.</li>
+    <li>Retire do forno e reserve até esfriar.</li>
+    <li>Em uma panela média, dissolva o amido de milho Maizena no leite, junte o açúcar e cozinhe em fogo médio, mexendo sempre, até ferver e engrossar.</li >
+    <li>Acrescente o chocolate e misture até obter uma consistência homogênea.</li>
+    <li>Reserve até amornar.</li >
+    <li>Coloque o recheio reservado sobre a massa, distribua as fatias de morango uma ao lado da outra e finalize polvilhando o chocolate ralado.</li >
+    <li>Leve à geladeira por 1 hora e sirva gelada.</li>
+
+</ul>
 </p>
 
 
