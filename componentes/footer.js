ac7e6ae53f1ff2c2e7e2aeab8500e4acd03f4337
leticia <lethy.ribi15@gmail.com>
2021-09-16 14:41:38 -0300

    novo

--- componentes/footer.js
+++ componentes/footer.js
@@ -1,11 +1,11 @@
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
-
+import Link from 'next/Link'
 function Footer(){
     return (
         <>
             <footer className={styles.footer}>
-                <a
+                <Link
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -14,7 +14,7 @@ function Footer(){
                 <span className={styles.logo}>
                     <Image src="/footer.png" alt="Vercel Logo" width={720} height={160} />
                 </span>
-                </a>
+                </Link>
             </footer>
         </>
     )
